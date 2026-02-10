4dac6dfae367abbd9d16c6fae630dbea8f75095a
Luis MX <ing.arellano@gmail.com>
2026-02-10 14:36:37 -0300

    Notas al proyecto

---
+++ README.md
@@ -0,0 +1,17 @@
+
+
+10-02-2026
+
+Flujo en este proyecto:
+
+Controller
+↓
+Application Service
+↓
+Domain (Entidad + reglas)
+↓
+Repository (interfaz)
+↓
+Adapter JPA
+↓
+PostgreSQL
--- src/main/java/com/laros/orders/OrdersApplication.java
+++ src/main/java/com/laros/orders/OrdersApplication.java
@@ -3,23 +3,6 @@ package com.laros.orders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-10-02-2026
-
-Flujo en este proyecto:
-
-Controller
-   ↓
-Application Service
-   ↓
-Domain (Entidad + reglas)
-   ↓
-Repository (interfaz)
-   ↓
-Adapter JPA
-   ↓
-PostgreSQL
- */
 @SpringBootApplication
 public class OrdersApplication {
 
